43494af14cccf0ac3369e21ff742463cd8a24ca4
crown1122m <153993332+crown1122m@users.noreply.github.com>
2025-02-05 14:10:07 -0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+
+menubar.onclick=()=>{
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
+}
+
+
+      
+      
